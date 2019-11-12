dede4952e2d9e8bc149f480f452c70ccf811ad77
Forrest-Darabian <mr.smiley72@yahoo.com>
2019-11-12 14:24:52 -0800

    middleware working!

--- users/users-router.js
+++ users/users-router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Users = require("./users-model.js");
 const restrict = require("../auth/restrict.js");
 
-router.get("/", (req, res) => {
+router.get("/", restrict, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
